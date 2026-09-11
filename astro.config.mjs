b429d71d35c9d32bb61b86fb27c178550615c111
玛特大叔 <dialox@uncle.mt>
2026-09-11 10:29:14 +0800

    chore: astro scaffold

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'zh'],
+    routing: { prefixDefaultLocale: false },
+  },
+});
