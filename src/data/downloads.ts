44519ce74f6926dd4440b67f7d5adf1095ea0741
玛特大叔 <dialox@uncle.mt>
2026-09-11 10:46:52 +0800

    feat: download page with platform cards (en/zh)

---
+++ src/data/downloads.ts
@@ -0,0 +1,6 @@
+export const RELEASES_URL = 'https://github.com/tokenmt/tokenhub-desktop/releases';
+export const platforms = [
+  { id: 'windows', labelKey: 'dl.windows' },
+  { id: 'macos', labelKey: 'dl.macos' },
+  { id: 'linux', labelKey: 'dl.linux' },
+] as const;
